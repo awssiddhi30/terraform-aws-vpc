05e4c473200cfbfebcfde0cc4682ca3ff5f11e1a
awssiddhi30 <psd.shardadevi30@gmail.com>
2025-04-20 12:54:14 +0530

    new file

--- locals.tf
+++ locals.tf
@@ -3,4 +3,8 @@ locals {
   az_names = slice(data.aws_availability_zones.available.names, 0, 2)
   default_vpc_id = data.aws_vpc.default.id
   default_vpc_cidr = data.aws_vpc.default.cidr_block
+  pub_route_id     = aws_route_table.public.id
+  pri_route_id     = aws_route_table.private.id
+  db_route_id      = aws_route_table.database.id
+
 }
